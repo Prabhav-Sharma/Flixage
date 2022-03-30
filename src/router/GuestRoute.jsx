5e0984eaced432d32a79f3069d054e301dd7af50
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-03-30 18:16:48 +0530

    Feat: Add router templates

---
+++ src/router/GuestRoute.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GuestRoute() {
+  return <div></div>;
+}
+
+export default GuestRoute;
---
+++ src/router/ProtectedRoutes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProtectedRoutes() {
+  return <div></div>;
+}
+
+export default ProtectedRoutes;
