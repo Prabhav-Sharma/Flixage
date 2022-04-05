914827f6dc95b4dba04065a06aef205900cc3455
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-04-05 10:26:31 +0530

    Feat: Add history page

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import { useEffect } from "react";
+import { useUserData } from "../../contexts/providers/UserDataProvider";
+import { useDocumentTitle } from "../../hooks";
+import {
+  fetchHistory,
+  addToHistory,
+  deleteFromHistory,
+  clearAllHistory,
+  fetchVideos,
+} from "../../services";
+import { AiOutlineDelete } from "react-icons/ai";
+import { MdOutlineExplore } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { VideoGrid } from "../../components";
+import styles from "./History.module.css";
+
+//temp import
+import { useVideos } from "../../contexts/providers/VideosProvider";
 
 function History() {
-  return <div>History</div>;
+  const {
+    state: { history },
+    dispatch: userDataDispatch,
+  } = useUserData();
+
+  const token = localStorage.getItem("token");
+  useDocumentTitle("History | Flixage");
+
+  useEffect(() => {
+    fetchHistory(token, userDataDispatch);
+    fetchVideos(setVideos);
+  }, []);
+
+  //temporary functions till addToHistory function is added on the single video page, which at the time, doesn't exist.
+  const { videos, setVideos } = useVideos();
+  const loadDummyHistory = async () => {
+    const dummyVideos = videos.splice(0, 4);
+    console.log(dummyVideos);
+    await dummyVideos.forEach((video) =>
+      addToHistory({ video: video }, token, userDataDispatch)
+    );
+  };
+
+  const historyItems =
+    history.length !== 0 ? (
+      <>
+        <div className={`${styles.wrapper} flex-col`}>
+          <button
+            className={`prim-btn ${styles.clear_btn}`}
+            onClick={() => clearAllHistory(token, userDataDispatch)}
+          >
+            Clear All <AiOutlineDelete className={styles.btn_icon} />
+          </button>
+        </div>
+        <VideoGrid videos={history} type={"HISTORY"} />
+      </>
+    ) : (
+      <div className={`${styles.wrapper} flex-col`}>
+        <h2 className={styles.no_history_msg}>
+          You must create History, in order to witness it.
+        </h2>
+        <button className={`prim-btn ${styles.explore_btn}`}>
+          <Link to="/videos" className={styles.link_style}>
+            Explore <MdOutlineExplore className={styles.btn_icon} />
+          </Link>
+        </button>
+        <button
+          onClick={() => loadDummyHistory()}
+          className={`prim-btn ${styles.dummy_btn}`}
+        >
+          Add Dummy History
+        </button>
+      </div>
+    );
+
+  return (
+    <section>
+      <h1 className={`${styles.page_heading} center`}>Watch History</h1>
+      {historyItems}
+    </section>
+  );
 }
 
 export default History;
