ff53ea2ef28266027980b650a9e2248b3627a6b1
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-04-07 11:28:20 +0530

    Feat: Add Playlists page

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,20 @@ import VideoCard from "./Card/VideoCard/VideoCard";
 import Navigation from "./Navigation/Navigation";
 import TextInput from "./TextInput/TextInput";
 import VideoGrid from "./VideoGrid/VideoGrid";
+import PlaylistModal from "./PlaylistModal/PlaylistModal";
+import HorizontalVideoCard from "./Card/HorizontalVideoCard/HorizontalVideoCard";
+import WatchLaterButton from "./ActionButtons//WatchLaterButton";
+import LikesButton from "./ActionButtons/LikesButton";
+import PlaylistButton from "./ActionButtons/PlaylistButton";
 
-export { VideoCard, Navigation, TextInput, VideoGrid };
+export {
+  VideoCard,
+  Navigation,
+  TextInput,
+  VideoGrid,
+  PlaylistModal,
+  HorizontalVideoCard,
+  WatchLaterButton,
+  LikesButton,
+  PlaylistButton,
+};
