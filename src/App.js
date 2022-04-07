2852b50402fbfcaefa5c71fd58557ca63d8d8abc
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-04-07 17:29:14 +0530

    Feat: Add ToastContainer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Navigation } from "./components";
 import Router from "./router/Router";
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <ToastContainer theme="dark" autoClose={2000} />
       <main>
         <Router />
       </main>
