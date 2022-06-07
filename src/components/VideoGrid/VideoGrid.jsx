3e406232489c93e7a5ad39fa67695398a10c625e
Prabhav-Sharma <prabhav.ps8@gmail.com>
2022-06-07 16:30:22 +0530

    Feat: Add Prop for center aligning items

--- src/components/VideoGrid/VideoGrid.jsx
+++ src/components/VideoGrid/VideoGrid.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import styles from "./VideoGrid.module.css";
 import { VideoCard } from "../index";
 
-function VideoGrid({ videos = [], classNames, type = "default" }) {
+function VideoGrid({
+  videos = [],
+  classNames,
+  type = "default",
+  alignCenter = false,
+}) {
   return (
-    <div className={`${styles.videos_wrapper} ${classNames}`}>
+    <div
+      className={`${styles.videos_wrapper} ${classNames} ${
+        alignCenter && styles.align_center
+      } `}
+    >
       {videos.map((video) => (
         <VideoCard key={video._id} video={video} type={type} />
       ))}
